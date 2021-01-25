a7df629b0c7dee589de13ede91c8b53c125c6f51
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-01-25 00:53:50 -0300

    -nav bar added -image carousel added

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const staticFolder = path.resolve(__dirname, 'public');
+const staticFolder = path.resolve(__dirname, '../public');
 app.use(express.static(staticFolder));
 
 app.get('/', (req, res) => {
      res.sendFile(path.resolve(__dirname, "../views/index.html"))
 })
+
 
 app.listen(8080, () => {
     console.log('Server Running')
